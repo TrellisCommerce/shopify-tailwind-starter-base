1f18e0cf8f071ab817367520df43cee55e45bc5d
m0k13 <45663017+m0k13@users.noreply.github.com>
2023-08-08 13:42:45 +0200

    new price display + total compare at rpice

--- assets/build-a-box.js
+++ assets/build-a-box.js
@@ -1,19 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Select all buttons
-  var optionButtons = document.getElementsByClassName('option-btn');
-  var planButtons = document.getElementsByClassName('plan-selector-btn');
+  const optionButtons = document.getElementsByClassName('option-btn');
+  const planButtons = document.getElementsByClassName('plan-selector-btn');
+  const priceTotalElement = document.querySelector('.price-total');
+  const priceTotalCompareAtContainer = document.querySelector(
+    '.price-display-compare',
+  );
+  const priceTotalCompareAtElement =
+    document.querySelector('.price-compare-at');
+  const quantityTotalElement = document.querySelector('.total-quantity-couunt');
 
-  var currentPlan = 689522049319;
+  let currentPlan = 689522049319;
 
   function getVariantItems() {
-    var variantElements = document.getElementsByClassName('variant');
-    var itemsToAdd = [];
+    const variantElements = document.getElementsByClassName('variant');
+    let itemsToAdd = [];
 
-    for (var i = 0; i < variantElements.length; i++) {
-      var variantElement = variantElements[i];
-      var variantId =
+    for (let i = 0; i < variantElements.length; i++) {
+      const variantElement = variantElements[i];
+      const variantId =
         variantElement.getElementsByClassName('variant-id')[0].value;
-      var quantity =
+      const quantity =
         variantElement.getElementsByClassName('variant-quantity')[0].value;
 
       itemsToAdd.push({
@@ -25,44 +32,56 @@ document.addEventListener('DOMContentLoaded', function () {
     return itemsToAdd;
   }
 
-  const priceTotalElement = document.querySelector('.price-total');
-
   function calculateTotal() {
     const itemsToAdd = getVariantItems();
-    console.log('calculating price');
     let totalPrice = 0;
     let totalPriceOriginal = 0;
+    let totalQuantity = 0;
+
     for (let i = 0; i < itemsToAdd.length; i++) {
       const item = itemsToAdd[i];
       const variant = window.ur_subscription_variants[item.id];
       let itemPriceOriginal = variant.price * item.quantity;
+      if (variant.compare_at_price) {
+        itemPriceOriginal = variant.compare_at_price * item.quantity;
+      }
       let itemPrice = itemPriceOriginal;
-
-      console.log(variant);
 
       if (currentPlan) {
         if (currentPlan != '') {
-          console.log(currentPlan);
           for (let j = 0; j < variant.selling_plan_allocations.length; j++) {
             const plan = variant.selling_plan_allocations[j];
-            console.log(plan);
             if (plan.selling_plan_id == currentPlan) {
               itemPrice = plan.price * item.quantity;
             }
           }
         }
       }
-
       totalPriceOriginal = totalPriceOriginal + itemPriceOriginal;
       totalPrice = totalPrice + itemPrice;
+      totalQuantity = totalQuantity + parseInt(item.quantity);
     }
-    totalPriceOriginal = totalPriceOriginal / 100;
-    totalPrice = totalPrice / 100;
-    priceTotalElement.innerHTML = totalPrice.toLocaleString(undefined, {
+
+    priceTotalElement.innerHTML = (totalPrice / 100).toLocaleString(undefined, {
       maximumFractionDigits: 2,
       minimumFractionDigits: 2,
     });
+
+    if (totalPrice != totalPriceOriginal) {
+      priceTotalCompareAtContainer.classList.remove('xhidden');
+      priceTotalCompareAtElement.innerHTML = (
+        totalPriceOriginal / 100
+      ).toLocaleString(undefined, {
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2,
+      });
+    } else {
+      priceTotalCompareAtContainer.classList.add('xhidden');
+    }
+
+    quantityTotalElement.innerHTML = totalQuantity;
   }
+  calculateTotal();
 
   //Send Cart Form
   document
@@ -72,7 +91,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
       itemsToAdd = getVariantItems();
 
-      var data = {
+      const data = {
         items: itemsToAdd,
       };
 
@@ -110,8 +129,10 @@ document.addEventListener('DOMContentLoaded', function () {
     const target = btn.nextElementSibling;
     let value = Number(target.value);
     value--;
-    target.value = value;
-    calculateTotal();
+    if (value >= 0) {
+      target.value = value;
+      calculateTotal();
+    }
   }
 
   function increment(e) {
@@ -142,7 +163,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   // Selling Plan Buttons
-  for (var i = 0; i < planButtons.length; i++) {
+  for (let i = 0; i < planButtons.length; i++) {
     planButtons[i].addEventListener('click', function (event) {
       currentPlan = this.getAttribute('data-plan');
       for (var j = 0; j < planButtons.length; j++) {
@@ -154,19 +175,19 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   // Variant Option Buttons
-  for (var i = 0; i < optionButtons.length; i++) {
+  for (let i = 0; i < optionButtons.length; i++) {
     optionButtons[i].addEventListener('click', function (event) {
-      for (var j = 0; j < optionButtons.length; j++) {
+      for (let j = 0; j < optionButtons.length; j++) {
         optionButtons[j].classList.remove('active');
       }
       this.classList.add('active');
 
-      var clickedOption = this.getAttribute('data-option');
+      const clickedOption = this.getAttribute('data-option');
 
-      var variants = document.getElementsByClassName('ur-variant');
+      const variants = document.getElementsByClassName('ur-variant');
 
-      for (var k = 0; k < variants.length; k++) {
-        var variant = variants[k];
+      for (let k = 0; k < variants.length; k++) {
+        const variant = variants[k];
 
         // If variant has the same data-option value as the clicked button,
         // remove 'hidden' class. Otherwise, add 'hidden' class and set quantity to 0.
