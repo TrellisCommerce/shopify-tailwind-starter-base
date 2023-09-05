c2b6862ed88b74d4929fe5752e65abf64e240cc9
m0k13 <45663017+m0k13@users.noreply.github.com>
2023-09-05 16:38:45 +0200

    custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,21 @@ module.exports = {
     color: {
       black: '#151515',
     },
+    colors: {
+      beige: {
+        DEFAULT: '#F2EEE3',
+        100: '#FFFFFF',
+        200: '#F2EEE3',
+        300: '#E0D7BD',
+        400: '#CEBF96',
+        500: '#BDA870',
+        600: '#A78F4E',
+        700: '#816E3C',
+        800: '#5A4E2A',
+        900: '#342D18',
+        950: '#211C0F',
+      },
+    }
   },
   plugins: [],
 };
