48885023f9f515e304d5cfb23a99319986d80f35
emayton7 <emayton@trellis.co>
2024-07-25 23:33:39 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   prefix: 'twcss-',
-  // safelist is added to provide all styles for design to add through the admin
   safelist: [
     {
-      pattern: /.*/,
-      variants: ['xs', 'sm', 'md', 'lg', 'xl', 'hover', 'group-hover'],
-    },
-  ],
-  // !important is added to override core Dawn styles for design
-  important: true,
+      pattern /.*/,
+      variants: ['xs', 'sm', 'md', 'lg', 'hover', 'group-hover']
+      },
+    ],
+important: true,
   content: [
     './layout/*.liquid',
     './templates/*.liquid',
@@ -16,29 +14,97 @@ module.exports = {
     './sections/*.liquid',
     './snippets/*.liquid',
   ],
-  theme: {
-    screens: {
-      sm: '320px',
+    theme: {
+  screens: {
+    sm: '320px',
       md: '750px',
-      lg: '990px',
-      xl: '1440px',
-      '2xl': '1920px',
-      pageMaxWidth: '1440px',
+        lg: '990px',
+          xlg: '1440px',
+            x2lg: '1920px',
+              pageMaxWidth: '1440px',
     },
-    extend: {
-      fontFamily: {
-        heading: 'var(--font-heading-family)',
+  // ITC Benguiat replaces Revans
+  // Futura replaces Fenomen
+  // More comments you want to leave for others!
+  fontFamily: {
+    'serif': ['ITC Benguiat'],
+      'sans': ['Futura']
+  },
+  fontSize: {
+    // Primary Body Copy - Body 12
+    sm: ['12px', {
+      lineHeight: '150%',
+      letterSpacing: '0',
+      fontWeight: '400',
+    }],
+      // Primary Body Copy - Body 14
+      base: ['14px', {
+        lineHeight: '150%',
+        letterSpacing: '0',
+        fontWeight: '400',
+      }]
+  },
+  colors: {
+    // Default
+    transparent: 'transparent',
+      current: 'currentColor',
+        white: '#ffffff',
+          black: '#000000',
+            // Primary
+            // The primary colors should be utilized for the majority of brand and its application from retail to web.
+            primary: {
+      red: '#E12727',
+        grey: '#898E8E',
       },
-      animation: {
-        fadeIn: 'fadeIn 2s ease-in forwards',
+    // Flavors
+    flavor: {
+      chocolate: '#AC6E39',
+        vanilla: '#ECD798',
+          blue: '#079ADA',
+            espresso: '#7F5EC7',
       },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 },
-        },
+    // Secondary
+    // The secondary palette should be utilized sparingly and for specific situations that call for seasonal and holiday-themed design or association.
+    secondary: {
+      orange: '#D24728',
+        red: '#9B0F17',
+          pink: '#A1316A',
+            green: '#00793F',
+              purple: '#3D3385',
+                blue: '#284196',
       },
+    // Greys
+    // The grayscale palette should be utilized as a neutral complement to the primary and secondary colors in situations that call for more than a basic black background. They also provide a tool to create visual hierarchy in branded documentation.
+    grey: {
+      light: '#DEDBD9',
+        DEFAULT: '#5C5957',
+          dark: '#464442',
+            darker: '#201E1D'
     },
   },
+  spacing: {
+    '8': '8px',
+      '16': '16px',
+        // Spacing between components for mobile
+        // Margins for mobile
+        '32': '32px',
+          // Spacing between components for tablet
+          // Margins for tablet
+          '48': '48px',
+            '64': '64px',
+              // Spacing between components for desktop
+              // Margins for desktop
+              '80': '80px',
+                '112': '112px',
+                  '144': '144px',
+    },
+  extend: {
+  },
+
   plugins: [],
+      };
+      
+    },
+  },
+plugins: [],
 };
